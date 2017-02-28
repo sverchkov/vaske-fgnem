e47aac5d5ea1ee362c9358500a3b361f410453bc
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2017-02-27 19:08:33 -0600

    Created package for viz.

---
+++ VisualizeFGNEM/R/fgnemToCytoscape.R
@@ -0,0 +1,7 @@
+#' Generate Cytoscape sif
+#'
+#' @param fgnem Results struct from FGNEM. Needs to contain a [-1,0,1] nxn matrix `adj` and list `egene.logprobs`` of 3xm matrices
+#' @param sif.file the file to write to
+fgnemToCytoscape = function( fgnem, sif.file ){
+  # TODO
+}
